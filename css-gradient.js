eb48e1598842204c96d2b749135e99ee062bdf4e
karencofre <karencofrecejas@gmail.com>
2020-07-19 20:44:04 -0400

    initial commit

---
+++ css-gradient.js
@@ -0,0 +1,18 @@
+window.CSS.registerProperty({
+  name: "--color-primary",
+  syntax: "<color>",
+  inherits: true,
+  initialValue: "#fffdee",
+});
+window.CSS.registerProperty({
+  name: "--color-secondary",
+  syntax: "<color>",
+  inherits: true,
+  initialValue: "#ffe8a0",
+});
+window.CSS.registerProperty({
+  name: "--color-tertiary",
+  syntax: "<color>",
+  inherits: true,
+  initialValue: "#ffbf00",
+});
